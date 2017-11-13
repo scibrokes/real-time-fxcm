b16817996e0a0834edcb25436e661421f8d6d0b3
®γσ, Eng (白戸則道) <englianhu@gmail.com>
2017-11-13 10:47:44 +0000

    effecient `DataCollection` App deployed.

--- DataCollection/app.R
+++ DataCollection/app.R
@@ -1,11 +1,14 @@
 require('shiny')
+require('shinyTime')
 #'@ require('rdrop2')
 require('magrittr')
 require('plyr')
 require('dplyr')
 require('stringr')
-require('rvest')
+require('data.table')
+#'@ require('rvest')
 require('quantmod')
+require('TFX')
 require('lubridate')
 require('ggplot2')
 require('DT')
@@ -56,33 +59,30 @@ ui <- shinyUI(fluidPage(
                               textOutput('currentTime')),
                             br(), 
                             p(strong('Latest FX Quotes:'),
-                              tableOutput('fxdata'))), 
+                              tableOutput('fxdata'), 
+                              checkboxInput('pause', 'Pause updates', FALSE))), 
                    tabPanel('Chart', 
                             h3('Real Time Chart'), 
                             p(strong(paste0('Current time (', zone, '):')),
                               textOutput('currentTime2')),
                             br(), 
-                            plotOutput("plotEURUSD"), 
+                            plotOutput("plotBidPrice"), 
                             tags$hr(),
-                            plotOutput("plotUSDJPY"), 
-                            tags$hr(),
-                            plotOutput("plotGBPUSD"), 
-                            tags$hr(),
-                            plotOutput("plotUSDCHF"), 
-                            tags$hr(),
-                            plotOutput("plotUSDCAD"), 
-                            tags$hr(),
-                            plotOutput("plotAUDUSD")), 
+                            plotOutput("plotAskPrice")), 
                    tabPanel('Data', 
                             h3('Data Download'), 
                             p(strong(paste0('Current time (', zone, '):')),
                               textOutput('currentTime3')),
                             p('The time zone of data in GMT.'), 
                             dataTableOutput('fxDataTable'), 
-                            p('Please becareful, once you click on "Reset" button, ', 
+                            p(strong('Refresh'), 'button will collect the latest dataset ', 
+                              '(time unit in seconds).'), 
+                            p('Please becareful, once you click on', 
+                              strong('Reset'), 'button, ', 
                               'all data will be lost. Kindly download the dataset ', 
                               'as csv format prior to reset it.'), 
                             actionButton('refresh', 'Refresh', class = 'btn-primary'), 
+                            downloadButton('downloadData', 'Download'), 
                             actionButton('reset', 'Reset', class = 'btn-danger')))), 
         
         tabPanel('Appendix', 
@@ -91,12 +91,16 @@ ui <- shinyUI(fluidPage(
                             h3('Speech'), 
                             p('I try to refer to the idea from below reference to create this web ', 
                               'application for data collection.'), 
-                            p('Kindly browse over', HTML("<a href='https://github.com/scibrokes/real-time-fxcm'>Real Time FXCM</a>"), 'for more information.'), 
+                            p(HTML("<a href='https://beta.rstudioconnect.com/content/3138/'>Q1App2</a>"), 
+                              '(', strong('Q1App2'), 'inside 2nd reference link at below', 
+                              strong('Reference'), 'tab) for algorithmic trading. Kindly browse over', 
+                              HTML("<a href='https://github.com/scibrokes/real-time-fxcm'>Real Time FXCM</a>"), 
+                              'for more information about high frequency algorithmic trading.'), 
                             br(), 
                             h3('Reference'), 
                             p('01. ', HTML("<a href='https://github.com/cran/TFX'>TFX r package</a>")), 
-                            p('02. ', HTML("<a href='https://finance.yahoo.com/'>YAHOO! finance</a>")), 
-                            p('03. ', HTML("<a href='https://github.com/scibrokes/real-time-fxcm'>Real Time FXCM</a>"))), 
+                            p('02. ', HTML("<a href='https://www.fxcmapps.com/apps/basic-historical-data-downloader/'>Basic Historical Data Downloader</a>")), 
+                            p('03. ', HTML("<a href='https://github.com/englianhu/binary.com-interview-question'>binary.com : Job Application - Quantitative Analyst</a>"))), 
                    
                    tabPanel('Author', 
                             h3('Author'), 
@@ -130,222 +134,80 @@ server <- shinyServer(function(input, output, session){
     as.character(now('Asia/Tokyo'))
   })
   
-  output$fxdata <- renderTable({
-    # Forces invalidation in 1000 milliseconds
-    invalidateLater(1000, session)
-    
-    if(exists('realData')) {
-      if(is.data.frame(realData) & (nrow(realData) > 0)) {
-        tail(realData, 1) %>% tbl_df %>% mutate(
-          `TimeStamp (GMT)` = format(`TimeStamp (GMT)`,'%Y-%m-%d %H:%M:%OS'))
-      }
-    }
+  fetchData <- reactive({
+    if (!input$pause)
+      invalidateLater(750)
+    qtf <- QueryTrueFX()
+    qtf %<>% mutate(TimeStamp = as.character(TimeStamp))
+    names(qtf)[6] <- 'TimeStamp (GMT)'
+    return(qtf)
   })
+  
+  output$fxdata <- renderTable({
+    fetchData()
+  }, digits = 5, row.names = FALSE)
   
   # Function to get new observations
-  get_new_data <- function(curr){
-    data <- tryCatch(getSymbols('EURUSD=X', from = today('GMT'), 
-                                auto.assign = FALSE), error=function(e){})
-    if(!is.null(data)) {
-      data %<>% Cl
-      
-      if(curr == 'EURUSD=X') {
-        names(data) <- 'EURUSD'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/EURUSD=X?p=EURUSD=X'
-      } else if(curr == 'JPY=X') {
-        names(data) <- 'USDJPY'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/JPY=X?p=JPY=X'
-      } else if(curr == 'GBPUSD=X') {
-        names(data) <- 'GBPUSD'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/GBPUSD=X?p=GBPUSD=X'
-      } else if(curr == 'CHF=X') {
-        names(data) <- 'USDCHF'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/CHF=X?p=CHF=X'
-      } else if(curr == 'CAD=X') {
-        names(data) <- 'USDCAD'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/CAD=X?p=CAD=X'
-      } else if(curr == 'AUDUSD=X') {
-        names(data) <- 'AUDUSD'
-        #'@ lnk <- 'https://finance.yahoo.com/quote/AUDUSD=X?p=AUDUSD=X'
-      } else {
-        stop('The selected currency is not in the range.')
-      }
-      
-      #'@ msg <- lnk %>% read_html %>% html_nodes(xpath = '//*[@id="quote-market-notice"]/span') %>% html_text
-      
-      index(data) <- now('GMT')
-      return(data)
+  get_new_data <- function(){
+    readLines('http://webrates.truefx.com/rates/connect.html')
     }
-  }
   
-  ## ----------------- Start EURUSD ---------------------------
-  # Initialize EURUSD
-  if(!file.exists('EURUSD.rds')) {
-    EURUSD <<- get_new_data('EURUSD=X')
-  } else {
-    EURUSD <<- readRDS('EURUSD.rds')
-  }
+  ## ----------------- Start fxData ---------------------------
+  # Initialize fxData
+  fxData <<- get_new_data()
   
-  # Function to update EURUSD, latest data will be showing upside.
-  update_EURUSD <- function(){
-    EURUSD <<- rbind(get_new_data('EURUSD=X'), EURUSD)#  %>% unique
-    saveRDS(EURUSD, 'EURUSD.rds')
-    return(EURUSD)
-  }
+  # Function to update fxData, latest data will be showing upside.
+  update_data <- function(){
+    fxData <<- rbind(get_new_data(), fxData)#  %>% unique
+    saveRDS(fxData, paste0(str_replace_all(now('GMT'), ':', 'T'), 'GMT.rds'))
+    }
   
-  # Plot the 60 most recent values
-  output$plotEURUSD <- renderPlot({
+  output$plotBidPrice <- renderPlot({
     invalidateLater(1000, session)
-    update_EURUSD()
+    update_data()
     
-    ggplot() + geom_line(aes(x = index(EURUSD), y = coredata(EURUSD)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('EUR / USD')
-  })
-  ## ------------------ End EURUSD ----------------------------
+    dt <- terms()
+    if(nrow(dt) > 40) dt <- tail(dt, 40)
+    
+    ggplot(data = dt, aes(x = TimeStamp, y = Bid.Price, 
+                               group = Symbol, colour = Symbol)) +
+      geom_line() + geom_point( size = 4, shape = 21, fill = 'white') + 
+      ggtitle('Real Time Graph 1 : Forex Bid Price')
+    })
   
-  ## ----------------- Start USDJPY ---------------------------
-  # Initialize USDJPY
-  if(!file.exists('USDJPY.rds')) {
-    USDJPY <<- get_new_data('JPY=X')
-  } else {
-    USDJPY <<- readRDS('USDJPY.rds')
-  }
-  
-  # Function to update USDJPY, latest data will be showing upside.
-  update_USDJPY <- function(){
-    USDJPY <<- rbind(get_new_data('JPY=X'), USDJPY)#  %>% unique
-    saveRDS(USDJPY, 'USDJPY.rds')
-    return(USDJPY)
-  }
-  
-  # Plot the 60 most recent values
-  output$plotUSDJPY <- renderPlot({
+  output$plotAskPrice <- renderPlot({
     invalidateLater(1000, session)
-    update_USDJPY()
+    update_data()
     
-    ggplot() + geom_line(aes(x = index(USDJPY), y = coredata(USDJPY)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('USD / JPY')
-  })
-  ## ------------------ End USDJPY ----------------------------
-  
-  ## ----------------- Start GBPUSD ---------------------------
-  # Initialize GBPUSD
-  if(!file.exists('GBPUSD.rds')) {
-    GBPUSD <<- get_new_data('GBPUSD=X')
-  } else {
-    GBPUSD <<- readRDS('GBPUSD.rds')
-  }
-  
-  # Function to update GBPUSD, latest data will be showing upside.
-  update_GBPUSD <- function(){
-    GBPUSD <<- rbind(get_new_data('GBPUSD=X'), GBPUSD)#  %>% unique
-    saveRDS(GBPUSD, 'GBPUSD.rds')
-    return(GBPUSD)
-  }
-  
-  # Plot the 60 most recent values
-  output$plotGBPUSD <- renderPlot({
-    invalidateLater(1000, session)
-    update_GBPUSD()
+    dt <- terms()
+    if(nrow(dt) > 40) dt <- tail(dt, 40)
     
-    ggplot() + geom_line(aes(x = index(GBPUSD), y = coredata(GBPUSD)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('GBP / USD')
+    ggplot(data = dt, aes(x = TimeStamp, y = Ask.Price, 
+                               group = Symbol, colour = Symbol)) +
+      geom_line() + geom_point( size = 4, shape = 21, fill = 'white') + 
+      ggtitle('Real Time Graph 2 : Forex Ask Price')
   })
-  ## ------------------ End GBPUSD ----------------------------
-  
-  ## ----------------- Start USDCHF ---------------------------
-  # Initialize USDCHF
-  if(!file.exists('USDCHF.rds')) {
-    USDCHF <<- get_new_data('CHF=X')
-  } else {
-    USDCHF <<- readRDS('USDCHF.rds')
-  }
-  
-  # Function to update USDCHF, latest data will be showing upside.
-  update_USDCHF <- function(){
-    USDCHF <<- rbind(get_new_data('CHF=X'), USDCHF)#  %>% unique
-    saveRDS(USDCHF, 'USDCHF.rds')
-    return(USDCHF)
-  }
-  
-  # Plot the 60 most recent values
-  output$plotUSDCHF <- renderPlot({
-    invalidateLater(1000, session)
-    update_USDCHF()
-    
-    ggplot() + geom_line(aes(x = index(USDCHF), y = coredata(USDCHF)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('USD / CHF')
-  })
-  ## ------------------ End USDCHF ----------------------------
-  
-  ## ----------------- Start USDCAD ---------------------------
-  # Initialize USDCAD
-  if(!file.exists('USDCAD.rds')) {
-    USDCAD <<- get_new_data('CAD=X')
-  } else {
-    USDCAD <<- readRDS('USDCAD.rds')
-  }
-  
-  # Function to update USDCAD, latest data will be showing upside.
-  update_USDCAD <- function(){
-    USDCAD <<- rbind(get_new_data('CAD=X'), USDCAD)#  %>% unique
-    saveRDS(USDCAD, 'USDCAD.rds')
-    return(USDCAD)
-  }
-  
-  # Plot the 60 most recent values
-  output$plotUSDCAD <- renderPlot({
-    invalidateLater(1000, session)
-    update_USDCAD()
-    
-    ggplot() + geom_line(aes(x = index(USDCAD), y = coredata(USDCAD)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('USD / CAD')
-  })
-  ## ------------------ End USDCAD ----------------------------
-  
-  ## ----------------- Start AUDUSD ---------------------------
-  # Initialize AUDUSD
-  if(!file.exists('AUDUSD.rds')) {
-    AUDUSD <<- get_new_data('AUDUSD=X')
-  } else {
-    AUDUSD <<- readRDS('AUDUSD.rds')
-  }
-  
-  # Function to update AUDUSD, latest data will be showing upside.
-  update_AUDUSD <- function(){
-    AUDUSD <<- rbind(get_new_data('AUDUSD=X'), AUDUSD)#  %>% unique
-    saveRDS(AUDUSD, 'AUDUSD.rds')
-    return(AUDUSD)
-  }
-  
-  # Plot the 60 most recent values
-  output$plotAUDUSD <- renderPlot({
-    invalidateLater(1000, session)
-    update_AUDUSD()
-    
-    ggplot() + geom_line(aes(x = index(AUDUSD), y = coredata(AUDUSD)), 
-                         colour = 'blue') + xlab('Time [Seconds]') + ylab('AUD / USD')
-  })
-  ## ------------------ End AUDUSD ----------------------------
+  ## ------------------ End fxData ----------------------------
   
   terms <- reactive({
     input$refresh
     
-    if(all(file.exists('EURUSD.rds'), 
-           file.exists('USDJPY.rds'), 
-           file.exists('GBPUSD.rds'), 
-           file.exists('USDCHF.rds'), 
-           file.exists('USDCAD.rds'), 
-           file.exists('AUDUSD.rds'))) {
+    if(any(file.exists(paste0(dir(pattern = '.rds'))))) {
       realData <<- llply(dir(pattern = '.rds'), readRDS)
-      realData <<- do.call(cbind, realData)
-      if(!is.null(realData)) realData %<>% na.locf #%>% na.omit
-      realData <<- realData %>% tbl_df %>% mutate(`TimeStamp (GMT)` = index(realData))
-      realData <<- cbind(realData[ncol(realData)], realData[-ncol(realData)])
-    } else {
-      NULL
+      realData <<- do.call(rbind, realData) %>% unique
+      realData <<- ldply(realData, ParseTrueFX) %>% unique
     }
   })
+  
+  # Downloadable csv
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('fxData.csv', sep = '')
+    },
+    content = function(file) {
+      fwrite(terms(), file, row.names = FALSE)
+    }
+  )
   
   observe({
     if(input$reset){
